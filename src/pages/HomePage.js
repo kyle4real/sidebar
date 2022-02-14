64d442f3e2ae9e9222b66bef7ba7a9b0c11424e4
Kyle Friel <businessfriel@gmail.com>
2022-02-13 19:41:23 -0500

    Home page grid layout with Card components.

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,29 @@
 import React from "react";
+import styled from "styled-components";
+import { v } from "../styles/variables";
+import Card from "../components/UI/Card/Card";
 
 const HomePage = () => {
-    return <h1>Home Page</h1>;
+    return (
+        <SHomeGrid>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+        </SHomeGrid>
+    );
 };
+
+const SHomeGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: ${v.mdSpacing};
+
+    > div:nth-child(5) {
+        grid-column: 1 / span 3;
+    }
+`;
 
 export default HomePage;
